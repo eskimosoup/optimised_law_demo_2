a9df49b5c31a53108e4d81e28f2e93cf46281cc2
Optimised server <support@eskimosoup.co.uk>
2015-12-04 14:47:03 +0000

    Fix for error messages and missing departments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,14 @@ class ApplicationController < ActionController::Base
   before_action :global_site_settings, :load_objects
 
   unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, with: -> { render_error(404) }
+    rescue_from Exception, with: -> { render_error(500) }
     rescue_from ActiveRecord::RecordNotFound, with: -> { render_error(404) }
     rescue_from ActionController::RoutingError, with: -> { render_error(404) }
   end
 
   def render_error(status)
     respond_to do |format|
-      format.html { render 'errors/404', status: status }
+      format.html { render "errors/#{status}", status: status }
       format.all { render nothing: true, status: status }
     end
   end
